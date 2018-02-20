7bab3eefb2791c4ebdff979c6affb9f801ca5822
RICK STEELE <patrickjsteele@gmail.com>
2018-02-20 09:16:19 -0800

    enter now submits

--- src/js/sorry.js
+++ src/js/sorry.js
@@ -4,6 +4,16 @@
 // I'm not building backend password validation for
 // a static site on this already tight timeline.
 
+// treat enter as submit
+$(document).ready(function(){
+	document.getElementById("password").addEventListener("keyup", function(event) {
+	    event.preventDefault();
+	    if (event.keyCode === 13) {
+	        document.getElementById("reveal").click();
+	    }
+	});
+});
+
 function sorry(){
     if(document.getElementById('password').value == 'google'){
       $('.password-box').remove();
